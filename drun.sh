599c3cfd835267c37c05b16c3b49353a134948a3
LucienZerger <lucien.zerger@gmail.com>
2018-02-10 08:24:35 +0000

    Update drun.sh

--- drun.sh
+++ drun.sh
@@ -1,1 +1,2 @@
-sudo docker run -it --rm --name minecraft-server -p 19132:19132 -p 25565:25565 minecraft-server /bin/bash
+#sudo docker run -it --rm --name minecraft-server -p 19132:19132 -p 25565:25565 minecraft-server /bin/bash
+sudo docker run --rm --name minecraft-server -p 25565:25565 minecraft-server
